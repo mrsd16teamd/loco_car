f97ca754dd675ba940336201f640ad9529a68c05
Cyrus Liu <xiyuanl1@andrew.cmu.edu>
2017-03-29 18:25:51 -0400

    ..

--- ilqr_loco/include/ilqr_planner.h
+++ ilqr_loco/include/ilqr_planner.h
@@ -18,13 +18,13 @@ ilqr_loco::TrajExecGoal iLQR_gen_traj(nav_msgs::Odometry x_cur, std::vector<doub
   // TODO convert orientation  from quaternion
   double theta = ___;
 
-  double x_current[10] = {x_cur.pose.pose.position.x, x_cur.pose.pose.position.y, theta,
+  double x0[10] = {x_cur.pose.pose.position.x, x_cur.pose.pose.position.y, theta,
                           x_cur.twist.twist.linear.x, x_cur.twist.twist.linear.y,
                           x_cur.twist.twist.angular.z,
                           x_cur.twist.twist.linear.x, 0, 0, 0}
 
-  double* x_des = &x_desired[0];
-  double* obs = obstacle_pos.data;
+  double* xDes = &x_des[0];
+  double* Obs = obstacle_pos.data;
 
   // TODO what is the output type?
   int N = T+1;
@@ -36,14 +36,6 @@ ilqr_loco::TrajExecGoal iLQR_gen_traj(nav_msgs::Odometry x_cur, std::vector<doub
   Traj.u = malloc(m*(N-1)*sizeof(double));
   // traj[0]: states, traj[1]: controls
   plan_trajectory(x0,xDes,Obs,50,&Traj);
-
-  ilqr_loco::TrajExecGoal goal;
-  // TODO put output from c-code into action message
-
-  return goal;
-
-  // TODO what is the output type?
-  sometype outputs = iLQR_mpc(x_current, x_desired, obs, T);
 
   ilqr_loco::TrajExecGoal goal;
   // TODO put output from c-code into action message
