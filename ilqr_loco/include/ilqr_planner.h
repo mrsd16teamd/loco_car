35266d3f812a744bff4393f948ab69bf9fa157ca
Kazu Otani <kazuotani14@gmail.com>
2017-04-02 16:48:08 -0700

    add some comments on ilqr

--- ilqr_loco/include/ilqr_planner.h
+++ ilqr_loco/include/ilqr_planner.h
@@ -18,7 +18,7 @@ extern "C"{
 ilqr_loco::TrajExecGoal iLQR_gen_traj(nav_msgs::Odometry x_cur, std::vector<double> x_des,
                                         std_msgs::Float32MultiArray obstacle_pos, int T)
 {
-
+  //Pre-process inputs - put them in format that C-code wants
   double theta = tf::getYaw(x_cur.pose.pose.orientation);
 
   double x0[10] = {x_cur.pose.pose.position.x, x_cur.pose.pose.position.y, theta,
@@ -33,14 +33,15 @@ ilqr_loco::TrajExecGoal iLQR_gen_traj(nav_msgs::Odometry x_cur, std::vector<doub
   int n = 10;
   int m = 2;
 
+  //Run iLQR trajectory generation
   struct trajectory Traj;
   Traj.x = (double *) malloc(n*N*sizeof(double));
   Traj.u = (double *) malloc(m*(N-1)*sizeof(double));
   // traj[0]: states, traj[1]: controls
   plan_trajectory(x0,xDes,Obs,T,&Traj);
 
-  ilqr_loco::TrajExecGoal goal;
-
+  //Post-process iLQR trajectory - put states and controls into format that action client wants.
+  ilqr_loco::TrajExecGoal goal;	
   for(int i=0; i<N; i++) {
    	nav_msgs::Odometry odom;
 
@@ -52,7 +53,6 @@ ilqr_loco::TrajExecGoal iLQR_gen_traj(nav_msgs::Odometry x_cur, std::vector<doub
     odom.twist.twist.linear.x = Traj.x[i*n+3]; // Ux
  	odom.twist.twist.linear.x = Traj.x[i*n+4]; // Uy
  	odom.twist.twist.angular.z = Traj.x[i*n+5]; // r
-
 	goal.traj.states.push_back(odom);
   }
 
