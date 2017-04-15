b5e9927b9be279862f4a3c075f6b77a6699bd2d4
ambansal1@gmail.com <ambansal1@gmail.com>
2017-04-14 20:04:33 -0400

    999 change

--- obstacle_detection/lidartracking/src/main.cpp
+++ obstacle_detection/lidartracking/src/main.cpp
@@ -228,8 +228,8 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 
    if( obstaclepresent == 0 )
    {    
-       mapframe.point.x = 9 ;
-       mapframe.point.y  = 0;
+       mapframe.point.x = 10 ;
+       mapframe.point.y  = 12 ;
        mapframe.point.z  = 0;
 
    } 
