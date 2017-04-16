07d2aaa99d23b3137d81c28a4eccefd380d3d4ad
Kazu Otani <kazuotani14@gmail.com>
2017-04-16 16:47:43 -0400

    fix cmd_vel_out

--- utilities/twist_mux/src/twist_mux.cpp
+++ utilities/twist_mux/src/twist_mux.cpp
@@ -60,7 +60,7 @@ TwistMux::TwistMux(int window_size)
   getTopicHandles(nh, nh_priv, "locks" , *lock_hs_ );
 
   /// Publisher for output topic:
-  cmd_pub_ = nh.advertise<geometry_msgs::Twist>("cmd_vel_out", 1);
+  cmd_pub_ = nh.advertise<geometry_msgs::Twist>("cmd_vel_out", 1, true);
 
   /// Diagnostics:
   diagnostics_ = boost::make_shared<diagnostics_type>();
