f9dd794e814e513161121b2cab49bbe25bb9113c
Kazu Otani <kazuotani14@gmail.com>
2017-04-16 19:50:34 -0400

    change getparam to ros_fatal

--- ilqr_loco/include/try_get_param.h
+++ ilqr_loco/include/try_get_param.h
@@ -5,6 +5,6 @@
 // * all nodes that use this have node handle named nh
 // * name is a string
 // * variable is a member variable name
-#define TRYGETPARAM(name, variable) if(!nh.getParam(name, variable)){ROS_ERROR("MISSING PARAMETER: %s.", name); ros::shutdown();}
+#define TRYGETPARAM(name, variable) if(!nh.getParam(name, variable)){ROS_FATAL("MISSING PARAMETER: %s.", name); ros::shutdown();}
 
 #endif
