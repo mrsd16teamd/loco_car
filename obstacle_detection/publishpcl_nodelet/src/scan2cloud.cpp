677bb11df5512236ea2a9d53280757900e2f7e6a
Aum Jadhav <aum.jadhav@gmail.com>
2017-04-16 04:20:12 -0400

    Memory optimizations

--- obstacle_detection/publishpcl_nodelet/src/scan2cloud.cpp
+++ obstacle_detection/publishpcl_nodelet/src/scan2cloud.cpp
@@ -31,12 +31,13 @@ namespace publishpcl_nodelet // @(namespace)
     ros::Subscriber scan_sub_;
     ros::Timer timer_;
 
-    double front_angle; // currently 90 degrees
-    double new_angle_min;
-    double new_angle_max;
-    int min_index;
-    int max_index;
+    double front_angle_; // currently 90 degrees
+    double new_angle_min__;
+    double new_angle_max_;
+    int min_index_;
+    int max_index_;
 
+    sensor_msgs::PointCloud2 cloud_;
     sensor_msgs::LaserScan scan_front_;
 
 
@@ -44,49 +45,45 @@ namespace publishpcl_nodelet // @(namespace)
       ros::NodeHandle nh = getNodeHandle();
       ros::NodeHandle& private_nh = getPrivateNodeHandle();
 
-      // timer_ = nh.createTimer(ros::Duration(1.0), boost::bind(& Scan2Cloud::timerCb, this));
       scan_sub_ = nh.subscribe<sensor_msgs::LaserScan> ("scan", 1, &Scan2Cloud::scanCallback, this);
       pcl_pub_ = private_nh.advertise<sensor_msgs::PointCloud2> ("scan_cloud", 1, false);
 
-      try{
-        nh.getParam("scan_clip_angle", front_angle);
-        std::cout << "front_angle = " << front_angle << std::endl;
+      if (nh.hasParam("my_param")){
+        nh.getParam("scan_clip_angle", front_angle_);
+        // std::cout << "front_angle_ = " << front_angle_ << std::endl;
       }
-      catch(...){
+      else{
         NODELET_INFO_STREAM("Need param scan_clip_angle!");
         ros::shutdown();
       }
 
-      new_angle_min = -front_angle/2;
-      new_angle_max = front_angle/2;
+      new_angle_min_ = -front_angle_/2;
+      new_angle_max_ = front_angle_/2;
 
       double old_angle = 2.35619449615;
       double increment = 0.00436332309619;
 
-      min_index = floor((old_angle-new_angle_max) / increment);
-      max_index = 1080 - min_index;
-      std::cout << "min_index = " << min_index << std::endl;
-      std::cout << "max_index = " << max_index << std::endl;
-
+      min_index_ = floor((old_angle-new_angle_max_) / increment);
+      max_index_ = 1080 - min_index_;
+    //   std::cout << "min_index_ = " << min_index_ << std::endl;
+    //   std::cout << "max_index_ = " << max_index_ << std::endl;
     };
 
-    void timerCb(const ros::TimerEvent& event){
-    // Using timers is the preferred 'ROS way' to manual threading
-    NODELET_INFO_STREAM("The time is now " << event.current_real);
-    }
-
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
-      scan_front_ = *scan;
-      scan_front_.angle_min = new_angle_min;
-      scan_front_.angle_max = new_angle_max;
-      std::vector<float> new_data(&scan->ranges[min_index],&scan->ranges[max_index]);
-      scan_front_.ranges = new_data;
+      scan_front_.header = scan->header;
+      scan_front_.angle_min = new_angle_min_;
+      scan_front_.angle_max = new_angle_max_;
+      scan_front_.angle_increment = scan->angle_increment;
+      scan_front_.time_increment = scan->time_increment;
+      scan_front_.scan_time = scan->scan_time;
+      scan_front_.range_min = scan->range_min;
+      scan_front_.range_max = scan->range_max;
+      scan_front_.ranges.assign(scan.begin()+min_index_, scan.begin()+max_index_);
 
-      sensor_msgs::PointCloud2 cloud;
-      projector_.projectLaser(scan_front_, cloud);
-      pcl_pub_.publish(cloud);
+      projector_.projectLaser(scan_front_, cloud_);
+      pcl_pub_.publish(cloud_);
     }
   };
-} // namespace publishpcl
+} // namespace publishpcl_nodelet
 
 PLUGINLIB_DECLARE_CLASS(publishpcl_nodelet, Scan2Cloud, publishpcl_nodelet::Scan2Cloud, nodelet::Nodelet);
